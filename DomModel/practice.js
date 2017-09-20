f6a2d265d7d9dd16d2420434788f872f3aa2c866
pnegi <prinegi100@gmail.com>
2017-09-20 19:43:11 +0530

    example using DOM

---
+++ DomModel/practice.js
@@ -0,0 +1,7 @@
+// document.body.innerHTML = "<h1>hello there!!</h1>";
+
+const myTag = document.getElementsByTagName("p");
+
+console.log(myTag[1].innerHTML);
+console.log(myTag);
+console.log(myTag[0].innerHTML= "Hello guyz... I am Anshika ");
