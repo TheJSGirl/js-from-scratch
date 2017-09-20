b8ee7b983e7e0082938a4245a445b028b4c836e1
pnegi <prinegi100@gmail.com>
2017-09-21 00:27:51 +0530

    list example

--- DomModel/practice.js
+++ DomModel/practice.js
@@ -1,17 +1,33 @@
-// document.body.innerHTML = "<h1>hello there!!</h1>";
+// // document.body.innerHTML = "<h1>hello there!!</h1>";
 
-const myTag = document.getElementsByTagName("p");
+// const myTag = document.getElementsByTagName("p");
 
-console.log(myTag[1].innerHTML);
-console.log(myTag);
-console.log(myTag[0].innerHTML= "Hello guyz... I am Anshika ");
+// console.log(myTag[1].innerHTML);
+// console.log(myTag);
+// console.log(myTag[0].innerHTML= "Hello guyz... I am Anshika ");
 
-const allSpan = document.getElementsByTagName("span");
+// const allSpan = document.getElementsByTagName("span");
 
-for(const prop of allSpan){
-  prop.innerHTML = "somebody";
+// for(const prop of allSpan){
+//   prop.innerHTML = "somebody";
+// }
+
+
+// const id = document.getElementById("identifier");
+// id.innerHTML = allSpan.length;
+
+//list example with getElementById
+
+const members = document.getElementById("member");
+
+const allMemberNames = document.getElementsByTagName('LI');
+
+console.log(allMemberNames);
+
+// allMemberNames[3].innerHTML = "Not BOB";
+
+for(let i of allMemberNames){
+  if(i.innerHTML === "Bob"){
+    i.innerHTML = "Not Bob"
+  }
 }
-
-
-const id = document.getElementById("identifier");
-id.innerHTML = allSpan.length;
